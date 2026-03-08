e9cdb965a89c46a8406f379a4a4348867d7c5840
DynamoHive <gursoy.ferda@outlook.be>
2026-03-09 00:14:39 +0100

    Create posts.py

---
+++ backend/backend/posts.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+posts = []
+
+@router.post("/posts/create")
+def create_post(content: str):
+    post = {"id": len(posts)+1, "content": content}
+    posts.append(post)
+    return post
+
+@router.get("/posts")
+def list_posts():
+    return posts
