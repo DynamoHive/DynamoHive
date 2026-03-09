1637ec303d97f8a373a5de36b46c60793c97d55a
DynamoHive <gursoy.ferda@outlook.be>
2026-03-09 11:21:17 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 from importlib import import_module
+
 from backend.data_pipeline import start_pipeline
+from backend.events import router as events_router
+from backend.posts import router as posts_router
+from backend.users import router as users_router
 
 app = FastAPI()
 
 # pipeline başlat
 start_pipeline()
 
+# AI modülü
 ai_module = import_module("ai-engine.recommendation")
+
+# router bağlantıları
+app.include_router(events_router)
+app.include_router(posts_router)
+app.include_router(users_router)
 
 @app.get("/")
 def home():
