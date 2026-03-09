fb2b1d7beed0a4d43cf580b0f093e599c021a6d8
DynamoHive <gursoy.ferda@outlook.be>
2026-03-09 10:29:25 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,12 @@
+from fastapi import FastAPI
+from importlib import import_module
+from backend.data_pipeline import start_pipeline
+
+app = FastAPI()
+
+# pipeline başlat
+start_pipeline()
+
 ai_module = import_module("ai-engine.recommendation")
 
 @app.get("/")
@@ -7,8 +16,3 @@ def home():
 @app.get("/feed")
 def get_feed(user_id: int = 1):
     return ai_module.recommend_posts(user_id)
-
-app.include_router(user_router)
-app.include_router(post_router)
-app.include_router(event_router)
-
