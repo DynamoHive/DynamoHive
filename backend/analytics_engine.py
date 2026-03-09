9829d933bc4010989f619bfdfeb94f4c2cdf9e9f
DynamoHive <gursoy.ferda@outlook.be>
2026-03-09 17:28:49 +0100

    Update analytics_engine.py

--- backend/analytics_engine.py
+++ backend/analytics_engine.py
@@ -1,31 +1,12 @@
-from collections import defaultdict
+events_processed = 0
 
-metrics = {
-    "users": set(),
-    "posts": defaultdict(int),
-    "events": defaultdict(int)
-}
+def register_event():
 
-def record_event(event):
-
-    user_id = event.get("user_id")
-    event_type = event.get("type")
-    post_id = event.get("post_id")
-
-    if user_id:
-        metrics["users"].add(user_id)
-
-    if event_type:
-        metrics["events"][event_type] += 1
-
-    if post_id:
-        metrics["posts"][post_id] += 1
-
+    global events_processed
+    events_processed += 1
 
 def get_metrics():
 
     return {
-        "total_users": len(metrics["users"]),
-        "events": dict(metrics["events"]),
-        "posts": dict(metrics["posts"])
+        "events_processed": events_processed
     }
