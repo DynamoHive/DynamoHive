de4adc811c37f042f5f9118c32701b3e56b3ddc8
DynamoHive <gursoy.ferda@outlook.be>
2026-03-09 17:15:52 +0100

    Update events.py

--- backend/events.py
+++ backend/events.py
@@ -1,26 +1,11 @@
 from fastapi import APIRouter
-from backend.data_pipeline import add_event
+from backend.data_pipeline import event_queue
 
 router = APIRouter()
 
-events = []
-
 @router.post("/events")
-def create_event(event_type: str, user_id: int, post_id: int | None = None):
+def collect_event(event: dict):
 
-    event = {
-        "type": event_type,
-        "user_id": user_id,
-        "post_id": post_id
-    }
+    event_queue.put(event)
 
-    events.append(event)
-
-    add_event(event)
-
-    return {"status": "event recorded", "event": event}
-
-
-@router.get("/events")
-def list_events():
-    return events
+    return {"status": "event received"}
