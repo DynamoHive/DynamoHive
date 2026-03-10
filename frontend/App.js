a148af50bebedcf1a6be0045bf55dd97f95c21b9
DynamoHive <gursoy.ferda@outlook.be>
2026-03-10 03:38:51 +0100

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <h1>DynamoHive</h1>
+
+      <p>Frontend working</p>
+
+    </div>
+
+  );
+
+}
+
+export default App;
