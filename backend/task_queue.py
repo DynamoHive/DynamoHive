3e54612e7124d5a54b6831b4c8dc4539dea55d52
DynamoHive <gursoy.ferda@outlook.be>
2026-03-11 18:15:56 +0100

    Update task_queue.py

--- backend/task_queue.py
+++ backend/task_queue.py
@@ -1,28 +1,32 @@
+import queue
+import threading
+
+task_queue = queue.Queue()
 
 
 def add_task(task, *args):
 
-    TASK_QUEUE.put((task, args))
+    task_queue.put((task, args))
 
 
 def worker():
 
     while True:
 
-        task, args = TASK_QUEUE.get()
+        task, args = task_queue.get()
 
         try:
             task(*args)
 
         except Exception as e:
-            print("task error:", e)
+            print("Task error:", e)
 
-        TASK_QUEUE.task_done()
+        task_queue.task_done()
 
 
-def start_workers(count=2):
+def start_workers(num=3):
 
-    for _ in range(count):
+    for _ in range(num):
 
         t = threading.Thread(target=worker)
         t.daemon = True
