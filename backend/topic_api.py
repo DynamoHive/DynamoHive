d03286835eaaa87b714d6b2839a6d0c642f6f867
DynamoHive <gursoy.ferda@outlook.be>
2026-03-11 16:32:13 +0100

    Create topic_api.py

---
+++ backend/topic_api.py
@@ -0,0 +1,30 @@
+from database.database import cursor
+
+
+def get_topics():
+
+    cursor.execute(
+
+        """
+        SELECT topic, COUNT(*) as count
+        FROM posts
+        GROUP BY topic
+        ORDER BY count DESC
+        """
+
+    )
+
+    rows = cursor.fetchall()
+
+    topics = []
+
+    for r in rows:
+
+        topics.append({
+
+            "topic": r[0],
+            "count": r[1]
+
+        })
+
+    return topics
