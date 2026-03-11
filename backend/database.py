9d3f339696566d36e604054ea08a27bf025ee7ee
DynamoHive <gursoy.ferda@outlook.be>
2026-03-11 22:44:10 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,51 @@
 import sqlite3
 
-conn = sqlite3.connect("dynamohive.db", check_same_thread=False)
+conn = None
 
-cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS posts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT,
-    user_id TEXT
-)
-""")
+def init_database():
+    global conn
 
-conn.commit()
+    conn = sqlite3.connect("dynamohive.db", check_same_thread=False)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS posts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT
+    )
+    """)
+
+    conn.commit()
+
+
+def insert_post(title, content):
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO posts(title,content) VALUES (?,?)",
+        (title, content)
+    )
+
+    conn.commit()
+
+
+def get_posts():
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id,title FROM posts ORDER BY id DESC")
+
+    return cursor.fetchall()
+
+
+def get_post(post_id):
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM posts WHERE id=?", (post_id,))
+
+    return cursor.fetchone()
