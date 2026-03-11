d3c65632b1228f36db7ab5d1cfc08c0e75578ad9
DynamoHive <gursoy.ferda@outlook.be>
2026-03-11 23:25:32 +0100

    Update growth_engine.py

--- ai_engine/growth_engine.py
+++ ai_engine/growth_engine.py
@@ -1,5 +1,14 @@
-def growth_engine():
+growth_map = {}
 
-    print("Growth engine running")
 
-    return True
+def update_growth(topic):
+
+    if topic not in growth_map:
+        growth_map[topic] = 1
+    else:
+        growth_map[topic] += 1
+
+
+def get_growth():
+
+    return growth_map
