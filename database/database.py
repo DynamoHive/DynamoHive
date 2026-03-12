509a76e5bf6e65451bde0a1f8a21872b9b5c4cfa
DynamoHive <gursoy.ferda@outlook.be>
2026-03-12 03:00:25 +0100

    Change database path and simplify init_database
    Updated database connection path and removed unused functions.

--- database/database.py
+++ database/database.py
@@ -1,26 +1,19 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db", check_same_thread=False)
-
-
-def get_cursor():
-    return conn.cursor()
-
+DB_PATH = "database/dynamohive.db"
 
 def init_database():
-
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS posts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT,
-            content TEXT,
-            topic TEXT,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-        """
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
+    """)
 
     conn.commit()
+    conn.close()
