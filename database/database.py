ebd223417be0743d1f30abe395040caabfb45df7
DynamoHive <gursoy.ferda@outlook.be>
2026-03-15 00:42:56 +0100

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,1 +1,14 @@
-from backend.database import *
+import sqlite3
+import os
+
+DB_PATH = "database/dynamohive.db"
+
+
+def get_connection():
+
+    os.makedirs("database", exist_ok=True)
+
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+
+    return conn
