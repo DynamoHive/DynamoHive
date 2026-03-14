9dc351cbcef169756145cfc918ec0a550a25ee0f
DynamoHive <gursoy.ferda@outlook.be>
2026-03-14 05:23:40 +0100

    Update database.py

--- database/database.py
+++ database/database.py
@@ -2,23 +2,13 @@ import sqlite3
 
 DB_PATH = "database/dynamohive.db"
 
+
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
+
 def get_cursor():
     conn = get_connection()
     return conn, conn.cursor()
-
-Sonra cursor kullanacağın yerlerde şu şekilde kullan:
-
-from database.database import get_cursor
-
-conn, cursor = get_cursor()
-
-cursor.execute("SELECT * FROM posts")
-
-rows = cursor.fetchall()
-
-conn.close()
