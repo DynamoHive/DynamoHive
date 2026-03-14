a3d834dea347d7eb1d9927e3e8537dca8707a847
DynamoHive <gursoy.ferda@outlook.be>
2026-03-14 07:11:50 +0100

    Update database.py

--- database/database.py
+++ database/database.py
@@ -5,20 +5,11 @@ DB_PATH = "database/dynamohive.db"
 
 
 def get_connection():
+
     os.makedirs("database", exist_ok=True)
 
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+
     conn.row_factory = sqlite3.Row
 
     return conn
-
-Bu dosyanın tek görevi:
-
-database bağlantısı vermek
-
-Kullanımı:
-
-from database.database import get_connection
-
-conn = get_connection()
-cursor = conn.cursor()
