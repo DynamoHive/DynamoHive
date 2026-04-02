77aa649234600767b4fbbff6fdb8542a9aaee8e2
DynamoHive <gursoy.ferda@outlook.be>
2026-04-02 22:44:54 +0200

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,11 +1,48 @@
 import sqlite3
-import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_PATH = os.path.join(BASE_DIR, "database", "dynamohive.db")
+DB_PATH = "database.db"
 
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def save_post(title, content):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO posts (title, content)
+    VALUES (?, ?)
+    """, (title, content))
+
+    conn.commit()
+    conn.close()
+
+
+def get_posts():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM posts ORDER BY id DESC")
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return [dict(row) for row in rows]
